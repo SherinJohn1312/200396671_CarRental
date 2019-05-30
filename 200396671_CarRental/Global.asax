8bd75fabf44f4c13e0cb9d12c7ed37fbc4fc1410
Sherinjohn96 <sjohn9679@gmail.com>
2019-05-29 20:47:24 -0400

    Add project files.

---
+++ 200396671_CarRental/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_200396671_CarRental.MvcApplication" Language="C#" %>
---
+++ 200396671_CarRental/Models/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _200396671_CarRental.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public String Brand { get; set; }
+        public String Model { get; set; }
+        public String Type { get; set; }
+        public int Year { get; set; }
+
+    }
+}
